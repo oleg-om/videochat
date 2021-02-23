5685265f771907a7f9381e782489839d968fcf23
Oleg Omelchenko <olegoriginal@yandex.ru>
2021-02-23 23:27:24 +0300

    login added

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,5 +3,6 @@ const userCtrl = require("../controllers/userCtrl");
 
 router.post("/register", userCtrl.register);
 router.post("/activation", userCtrl.activateEmail);
+router.post("/login", userCtrl.login);
 
 module.exports = router;
