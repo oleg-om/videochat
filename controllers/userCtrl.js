f9d6c9d50b16146bbdee408b4c3e6f22191ef2bf
Dell <olegoriginal@yandex.ru>
2021-02-23 17:27:13 +0300

    registration done

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,15 +1,70 @@
 const Users = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jst = require("jsonwebtoken");
+
+const { CLIENT_URL } = process.env;
 
 const userCtrl = {
   register: async (req, res) => {
     try {
       const { name, email, password } = req.body;
-      console.log({ name, email, password });
-      res.json({ msg: "Register test" });
+      if (!name || !email || !password)
+        return res.status(400).json({ msg: "Please fill in all fields" });
+
+      if (!validateEmail(email))
+        return res.status(400).json({ msg: "Invalid email" });
+      const user = await Users.findOne({ email });
+      if (user)
+        return res.status(400).json({ msg: "This email already exists" });
+
+      if (password.length < 6)
+        return res
+          .status(400)
+          .json({ msg: "Passowrd must be at least 6 characters" });
+
+      const pawwordHash = await bcrypt.hash(password, 12);
+
+      const newUser = {
+        name,
+        email,
+        password: pawwordHash,
+      };
+
+      const activation_token = createActivationToken(newUser);
+
+      const url = `${CLIENT_URL}/user/activate/${activation_token}`;
+      //   sendMail(email, url);
+
+      res.json({
+        msg: "Register success! Please activate your email to start.",
+      });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
   },
+};
+
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+const createActivationToken = (payload) => {
+  return JsonWebTokenError.sign(payload, process.env.ACTIVATION_TOKEN_SECRET, {
+    expiresIn: "5m",
+  });
+};
+
+const createAccessToken = (payload) => {
+  return JsonWebTokenError.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+};
+
+const createRefreshToken = (payload) => {
+  return JsonWebTokenError.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
 };
 
 module.exports = userCtrl;
